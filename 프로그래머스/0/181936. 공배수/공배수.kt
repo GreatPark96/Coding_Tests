b4088be342cf2192710285ed37739fa680777890
PARK JI HWAN <38850652+GreatPark96@users.noreply.github.com>
2024-08-14 00:46:23 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 59.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(number: Int, n: Int, m: Int): Int {
+       
+        return when{
+            (number % n) == 0 && (number % m) == 0 -> {
+                1
+            }else -> {
+                0
+            }
+        }
+    }
+}
