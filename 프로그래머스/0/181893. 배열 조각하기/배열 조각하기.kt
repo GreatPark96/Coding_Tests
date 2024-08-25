7ae065f10600b9715782401a0655b63559fc12b0
PARK JI HWAN <38850652+GreatPark96@users.noreply.github.com>
2024-08-26 00:51:09 +0900

    [level 0] Title: 배열 조각하기, Time: 16.89 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(arr: IntArray, query: IntArray): IntArray {
+        var answer: IntArray = arr.clone()
+
+        for (i in query.indices) {
+            answer = if (i % 2 == 0) answer.sliceArray(0 .. query[i])
+                else answer.sliceArray(query[i]..answer.lastIndex)    
+        }
+        return answer
+    }
+}
